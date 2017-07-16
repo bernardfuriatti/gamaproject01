3598ef0ed9fee72d199f721dca4dabdf4fb52706
dti <dti@MacBook-Pro-de-Mariana.local>
2017-07-16 01:26:19 -0300

    Segunda modificacoes

--- js/app.js
+++ js/app.js
@@ -3,20 +3,21 @@ $( document ).ready(function() {
 
     firebase.auth().signInWithPopup(provider).then(function(result) {
         console.log("dey")
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      var token = result.credential.accessToken;
-      // The signed-in user info.
-      var user = result.user;
-      // ...
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        
+        // ...
     }).catch(function(error) {
         console.log('erro', error)
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      // The email of the user's account used.
-      var email = error.email;
-      // The firebase.auth.AuthCredential type that was used.
-      var credential = error.credential;
-      // ...
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
     });
 });
